24e48655aeab29c43fba63d04fbbb3f278dce86b
Volkan Yaşar Ustaoğlu <v.y.ustaoglu@gmail.com>
2022-09-26 00:25:12 +0300

    initial

--- ProjectApp.Core/Models/Product.cs
+++ ProjectApp.Core/Models/Product.cs
@@ -13,7 +13,6 @@ namespace ProjectApp.Core.Models
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
-        public int My { get; set; }
 
     }
 }
